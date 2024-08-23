d3e44956b642836055e5ed886ec4fd71ed4cd8d9
Marvin Baesa <gamalielpianojr@gmail.com>
2024-08-23 10:26:39 +0800

    add config

--- RealestateNZDash.py
+++ RealestateNZDash.py
@@ -19,13 +19,13 @@ st.markdown(
     .viewerBadge_text__1JaDK {
         display: none;
     }
-    header {visibility: hidden;}
+    
     footer {visibility: hidden;}
     </style>
     """,
     unsafe_allow_html=True
 )
-
+#header {visibility: hidden;}
 # Add custom CSS to set background and sidebar colors
 st.markdown(
     """
